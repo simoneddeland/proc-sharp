3dc31c1849f7599d0200012f17a249918b33bd4f
Sinon Eddeland <eddeland@gmail.com>
2019-10-10 21:09:08 +0200

    Initial commit

---
+++ src/ProcSharp/Initializer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SDL2;
+using static SDL2.SDL;
+
+namespace ProcSharpCore
+{
+    class Initializer
+    {
+
+        internal void Initialize(ref IntPtr window, ref IntPtr renderer)
+        {
+            if (SDL_Init(SDL_INIT_VIDEO) < 0)
+            {
+                throw new Exception(SDL_GetError());
+            }
+
+            window = SDL_CreateWindow("ProcSharp",
+                SDL_WINDOWPOS_CENTERED,
+                SDL_WINDOWPOS_CENTERED,
+                1020,
+                800,
+                SDL_WindowFlags.SDL_WINDOW_RESIZABLE
+            );
+
+            renderer = SDL_CreateRenderer(window, -1, SDL_RendererFlags.SDL_RENDERER_ACCELERATED);
+            SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+
+            if (window == IntPtr.Zero)
+            {
+                throw new Exception($"Unable to create a window. SDL. Error: {SDL_GetError()}");
+            }
+
+
+        }
+    }
+}
---
+++ src/ProcSharp/ProcSharp.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using SDL2;
+using static SDL2.SDL;
+
+namespace ProcSharpCore
+{
+    public class ProcSharp
+    {
+        private static IntPtr window;
+        private static IntPtr renderer;
+
+        private static Type gameType;
+        private static object gameObject;
+
+        private static bool quit = false;
+
+        private static Initializer initializer;
+
+        public static void Initialize(Type type, object game)
+        {
+            gameType = type;
+            gameObject = game;
+
+            initializer = new Initializer();
+            initializer.Initialize(ref window, ref renderer);
+
+            var userSetup = gameType.GetMethod("Setup");
+
+            // Call setup from the user
+            userSetup?.Invoke(gameObject, null);
+
+            SDL_RenderClear(renderer);
+            SDL_RenderPresent(renderer);
+
+            // Start the main loop
+            MainLoop();
+        }
+
+        private static void MainLoop()
+        {
+
+            var userDraw = gameType.GetMethod("Draw");
+            SDL_Event e;
+
+            while (!quit)
+            {
+                while (SDL_PollEvent(out e) != 0)
+                {
+                    switch (e.type)
+                    {
+                        case SDL_EventType.SDL_QUIT:
+                            quit = true;
+                            break;
+
+                        case SDL_EventType.SDL_KEYDOWN:
+                            switch (e.key.keysym.sym)
+                            {
+                                case SDL_Keycode.SDLK_q:
+                                    quit = true;
+                                    break;
+                            }
+                            break;
+                    }
+
+                    // Call the users draw function
+                    userDraw?.Invoke(gameObject, null);
+                    SDL_RenderPresent(renderer);
+
+                }
+            }
+
+            InternalExit();
+        }
+
+        private static void InternalExit()
+        {
+            // CLEANUP
+            SDL_DestroyRenderer(renderer);
+            SDL_DestroyWindow(window);
+            SDL_Quit();
+        }
+
+
+
+        #region Public methods
+        public static void Color(float r, float g, float b, float a)
+        {
+            SDL_SetRenderDrawColor(renderer, Convert.ToByte(r), Convert.ToByte(g), Convert.ToByte(b), Convert.ToByte(a));
+        }
+
+
+
+        public static void Exit()
+        {
+            quit = true;
+        }
+
+        public static int MouseX
+        {
+            get
+            {
+                int x;
+                int y;
+                SDL_GetMouseState(out x, out y);
+                return x;
+            }
+        }
+
+        public static int MouseY
+        {
+            get
+            {
+                int x;
+                int y;
+                SDL_GetMouseState(out x, out y);
+                return y;
+            }
+        }
+
+        public static void Square(float x, float y, float extent)
+        {
+            SDL_Rect drawRect;
+            drawRect.x = (int)x;
+            drawRect.y = (int)y;
+            drawRect.h = (int)extent;
+            drawRect.w = (int)extent;
+            SDL_RenderDrawRect(renderer, ref drawRect);
+        }
+
+        public static void Background(float v1, float v2, float v3)
+        {
+            byte currentRed;
+            byte currentGreen;
+            byte currentBlue;
+            byte currentAlpha;
+            SDL_GetRenderDrawColor(renderer, out currentRed, out currentGreen, out currentBlue, out currentAlpha);
+            SDL_SetRenderDrawColor(renderer, Convert.ToByte(v1), Convert.ToByte(v2), Convert.ToByte(v3), 255);
+            SDL_RenderClear(renderer);
+            SDL_SetRenderDrawColor(renderer, currentRed, currentGreen, currentBlue, currentAlpha);
+
+        }
+
+        #endregion
+    }
+}
