f5cdd5222827dcc8ff63a226c160627abab09dc1
Sinon Eddeland <eddeland@gmail.com>
2019-10-18 21:29:15 +0200

    Added a template that is published as a Visual Studio Extension

---
+++ template/ProcSharpGame/ProcSharpGame/Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ProcSharpCore;
+using static ProcSharpCore.ProcSharp;
+
+namespace ProcSharpGame
+{
+    class Game
+    {
+        public void Setup()
+        {
+
+        }
+
+        public void Draw()
+        {
+
+        }
+
+    }
+}
