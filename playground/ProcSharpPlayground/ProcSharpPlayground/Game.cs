ce8abe30859e07d6c96f824680131f2df4ce2035
Sinon Eddeland <eddeland@gmail.com>
2019-10-23 22:31:09 +0200

    Added a "Playground project" for easy access to trying out ProcSharp from source

---
+++ playground/ProcSharpPlayground/ProcSharpPlayground/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ProcSharpCore;
+using static ProcSharpCore.ProcSharp;
+
+namespace ProcSharpGame
+{
+    class Game
+    {
+        public void Setup()
+        {
+            Size(800, 600);
+        }
+
+        public void Draw()
+        {
+            Background(255, 255, 255);
+            Line(100, 100, MouseX, MouseY);
+        }
+
+    }
+}
---
+++ playground/ProcSharpPlayground/ProcSharpPlayground/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ProcSharpCore;
+
+namespace ProcSharpGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcSharp.Initialize(typeof(Game), new Game());
+        }
+    }
+}
